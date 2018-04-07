3f97793a00601e4e36f02f2781546e59037fbd38
vishwakftw <cs15btech11043@iith.ac.in>
2018-04-07 14:57:37 +0530

    Add confusion matrix plotting and update pipeline/README.md

--- pipeline/README.md
+++ pipeline/README.md
@@ -7,6 +7,8 @@
 + After this, we will obtain the top _K_ users of each "large" community. By default, "large" means communities with size greater than or equal to 50 and _K_ is 45. This is done using PageRank.
 + We then use [Python-Wikipedia](https://pypi.python.org/pypi/wikipedia) to extract the pages of the top people. The script used is `page_data.py`.
 + This is pre-processed using [NLTK](https://www.nltk.org/) using the script `data_preprocess.py`.
++ The pre-processed data is now fed to `classifier.py` for classification task. We use [Scikit-Learn](http://scikit-learn.org/stable/index.html) for the classifiers. You need to provide the splits of data in two files - one for training and one for testing. Every line in each of those files should follow this structure: `<comm_id> [\t person_id]`.
+    + At the end you can choose to only see the accuracy or you can only visualize the confusion matrix using [MatplotLib](https://matplotlib.org/) also.
 
 #### Instructions for using the code
 ------------------------------------
@@ -27,5 +29,11 @@ python page_data.py [-h] [--root ROOT] --communities COMMUNITIES
 ```
 python data_preprocess.py [-h] [--dir_root RAW DATA ROOT] [--verbose]
                           [--clean_data EMPTY_DIR_FOR_OUTPUT] [--stem]
+```
++ For `classifier.py`, the options are:
+```
+python classifier.py [-h] [--clean_data_root CLEAN_DATA_ROOT] [--train TRAIN]
+                     [--test TEST] [--op OP] [--show_confusion_matrix]
+                     [--verbose]
 ```
 + For information about the options can be found using the `-h` tag.
--- pipeline/classifier.py
+++ pipeline/classifier.py
@@ -10,9 +10,9 @@ Input:
     ...
 path of clean data
 
-Note: The order of cmmunities in trainset / testset does not matter,
-ie. first line can be : 25 ID1.txt ID2.txt etc. but it numbering of
-communites must follow the order 0, 1, .. n, where (n+1 would be
+Note: The order of communities in trainset / testset does not matter,
+i.e. first line can be : 25 ID1.txt ID2.txt etc. but the numbering of
+communites must follow the order 0, 1, .. n, where (n + 1 would be
 the number of lines in file)
 
 Output:
@@ -22,18 +22,40 @@ Accuracy on test set
 from __future__ import print_function
 from __future__ import division
 import os
-import string
 from argparse import ArgumentParser as AP
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import mutual_info_classif
 import numpy as np
+from matplotlib import pyplot as plt
 
 def _lines_in_file(file_path):
     with open(file_path, 'r') as f:
         for i, _ in enumerate(f):
             pass
     return i + 1
+
+def plot_confusion_matrix(cm, n_classes):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.YlOrBr)
+    plt.title('Confusion matrix')
+    plt.colorbar()
+    tick_marks = np.arange(n_classes)
+    plt.xticks(tick_marks, np.arange(n_classes), rotation=45)
+    plt.yticks(tick_marks, np.arange(n_classes))
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j]),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
     
 p = AP()
 p.add_argument('--clean_data_root', type=str, default='./clean_data',
@@ -43,6 +65,7 @@ p.add_argument('--train', type=str, default='./trainset.txt',
 p.add_argument('--test', type=str, default='./testset.txt',
                 help='Testset file')
 p.add_argument('--op', type=str, default='../OUTPUTS/classifier_acc.txt', help='Path of output file')
+p.add_argument('--show_confusion_matrix', action='store_true', help='Option to show confusion matrix')
 p.add_argument('--verbose', action='store_true',
                 help='option to print information at regular intervals | not helpful for large graphs')
 p = p.parse_args()
@@ -60,18 +83,17 @@ with open(trainset, 'rt') as trainfile:
         people = line.split()
         train[int(people[0])] = people[1:]
 
-# print(train)
 train_x = []
 train_y = []   
 for i in range(len(train)):
-    dir_path = os.path.join(clean_data, "community"+str(i))
+    dir_path = os.path.join(clean_data, "community" + str(i))
     for j in train[i]:
-        filepath = os.path.join(dir_path, j+".txt")
+        filepath = os.path.join(dir_path, j + ".txt")
         if os.path.exists(filepath):
             train_x.append(filepath)
             train_y.append(i)
         else:
-            print("[TRAIN SET] community"+str(i), j+".txt", "not found.")
+            print("[TRAIN SET] community" + str(i), j + ".txt", "not found.")
         
 with open(testset, 'rt') as testfile:
     for line in testfile:
@@ -83,14 +105,14 @@ with open(testset, 'rt') as testfile:
 test_x = []
 test_y = []   
 for i in range(len(test)):
-    dir_path = os.path.join(clean_data, "community"+str(i))
+    dir_path = os.path.join(clean_data, "community" + str(i))
     for j in test[i]:
-        filepath = os.path.join(dir_path, j+".txt")
+        filepath = os.path.join(dir_path, j + ".txt")
         if os.path.exists(filepath):
             test_x.append(filepath)
             test_y.append(i)
         else:
-            print("[TEST SET] community"+str(i), j+".txt", "not found.")
+            print("[TEST SET] community" + str(i), j + ".txt", "not found.")
 
 if verbose:
     print("Reading train and test file complete")
@@ -100,7 +122,8 @@ selector = SelectKBest(mutual_info_classif, k=100)
 
 train_x_tf = vectorizer.fit_transform(train_x)
 train_x_stf = selector.fit_transform(train_x_tf, train_y)
-# print(train_x_stf.shape)
+if verbose:
+    print("Shape of training data: {}".format(train_x_stf.shape))
 
 from sklearn.naive_bayes import MultinomialNB
 clf = MultinomialNB().fit(train_x_stf, train_y)
@@ -110,11 +133,19 @@ if(verbose):
     
 test_x_tf = vectorizer.transform(test_x)
 test_x_stf = selector.transform(test_x_tf)
-# print(test_x_stf.shape)
+if verbose:
+    print(" Shape of testing data: {}".format(test_x_stf.shape))
 pred = clf.predict(test_x_stf)
 
-print("Testset accuracy obtained: ", end='')
+print("Testing set accuracy obtained: ", end='')
 acc = np.mean(pred == test_y)
 print(acc)
 with open(p.op, 'wt') as f:
     f.write("Testset accuracy obtained: " + str(acc))
+
+if p.show_confusion_matrix:
+    from sklearn.metrics import confusion_matrix
+    cnf_mat = confusion_matrix(test_y, pred)
+    plt.figure()
+    plot_confusion_matrix(cnf_mat, len(train))
+    plt.show()
